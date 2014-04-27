f6786df06c3792cb215189b2f81efd97efe80065
Vikki <vikki.read@gmail.com>
2014-04-27 13:57:35 +0100

    parsing the har now so can do prettier things looking for requests #hubbah :P

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ proxy.doHAR(testUrl, function(err, data) {
     if (err) {
         console.error('ERROR: ' + err);
     } else {
-    	var hazImpreshun = data.indexOf('t=imp') !== -1;
+		var har = JSON.parse(data);
+		
+		function findMatchingEntries(har, stringToMatch) {
+			var harEntries = har.log.entries;
+			return harEntries.filter(function(entry) {
+				return entry.request.url.indexOf(stringToMatch) !== -1;
+			});
+		}
+
+    	var hazImpreshun = findMatchingEntries(har, "t=imp").length > 0;
     	if (hazImpreshun) {
     		console.log('i can haz impreshun!');
     	} else {
@@ -24,3 +33,4 @@ proxy.doHAR(testUrl, function(err, data) {
         //fs.writeFileSync(harName, data, 'utf8');
     }
 });
+
